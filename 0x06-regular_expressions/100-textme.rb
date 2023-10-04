d35258c4d6e7519af787a839cc19029c3e36fdc3
ondanje <cyrilondanje@gmail.com>
2023-10-04 18:43:27 +0300

    Parsing texts using regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #This exercise was prepared for you by Guillaume Plessis,
-VP of Infrastructure at TextMe.
+#VP of Infrastructure at TextMe.
 #It is something he uses daily. You can thank Guillaume for his project on Twitter.
 
 puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
