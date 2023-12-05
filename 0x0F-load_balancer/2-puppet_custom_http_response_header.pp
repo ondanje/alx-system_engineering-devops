c38a2bc423ddcaacaf98c00c245d38796228c697
ondanje <cyrilondanje@gmail.com>
2023-12-05 23:29:40 +0300

    using puppet to configure

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,11 +2,28 @@
 # install and configure an Nginx server using Puppet
 # instead of Bash
 
-exec { 'command':
-  command  => ' sudo apt-get -y update;
-  sudo apt-get install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;"\
-  /etc/nginx/sites-available/default;
-  service nginx restart',
-  provider => shell,
+
+package { 'nginx':
+  ensure => 'installed',
+  name   => 'nginx',
+}
+
+# Define the nginx configuration file path
+$nginx_conf = '/etc/nginx/nginx.conf'
+
+# Add a line after the 'http {' section in nginx.conf
+augeas { 'add_custom_header':
+  context => "/files${nginx_conf}/",
+  changes => [
+    "ins http after server[last()]",
+    "set http/add_header X-Served-By ${hostname}",
+  ],
+  onlyif  => "match http[last()]/add_header X-Served-By[. = '${hostname}'] size == 0",
+  require => Package['nginx'], # Ensure that nginx is installed before modifying the configuration
+}
+
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => Augeas['add_custom_header'], # Subscribe to the Augeas resource
 }
