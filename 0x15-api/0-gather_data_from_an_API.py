55cdfc95a16e922da81395045db2f7f1cab61b71
ondanje <cyrilondanje@gmail.com>
2024-01-02 21:35:23 +0300

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API,
+for a given employee ID,returns information
+about his/her TODO list
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    employee_Id = sys.argv[1]
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_Id)
+    response = requests.get(url)
+    name = response.json().get('name')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.\
+        format(employee_Id)
+    response = requests.get(url)
+    tasks = response.json()
+    done = 0
+    completed_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            completed_tasks.append(task)
+            done += 1
+
+    print('Employee {} is done with tasks({}/{})'.
+          format(name, done, len(tasks)))
+
+    for task in completed_tasks:
+        print('\t {}'.format(task.get('title')))
