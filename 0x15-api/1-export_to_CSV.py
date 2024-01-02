23a72ad97196f77222e7a0c35dbd23c4da142734
ondanje <cyrilondanje@gmail.com>
2024-01-02 21:52:00 +0300

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,16 +4,16 @@ Python script that, using this REST API,
 for a given employee ID,returns information
 about his/her TODO list
 """
-import sys
-import requests
 import csv
+import requests
+import sys
 
 if __name__ == "__main__":
     employee_Id = sys.argv[1]
 
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_Id)
     response = requests.get(url)
-    name = response.json()
+    name = response.json().get('name')
 
     url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.\
         format(employee_Id)
@@ -25,5 +25,5 @@ if __name__ == "__main__":
         csv_writer = csv.writer(csv_file)
 
         for task in tasks:
-            csv_writer.writerow([employee_Id, name.get('name'),
+            csv_writer.writerow([employee_Id, name,
                                  task.get('completed'), task.get('title')])
