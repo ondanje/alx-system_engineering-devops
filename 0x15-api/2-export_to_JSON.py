51a6620e4b99542717b3f8d0c8d4fae64762e688
ondanje <cyrilondanje@gmail.com>
2024-01-02 21:36:35 +0300

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Export to JSON
+"""
+import sys
+import requests
+import json
+
+if __name__ == "__main__":
+    employee_Id = sys.argv[1]
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_Id)
+    response = requests.get(url)
+    name = response.json().get('name')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.\
+        format(employee_Id)
+    response = requests.get(url)
+    tasks = response.json()
+    dict = {employee_Id: []}
+    for task in tasks:
+        dict[employee_Id].append({
+                                    "task": task.get('title'),
+                                    "completed": task.get('completed'),
+                                    "name": name
+                                    })
+    with open('{}.json'.format(employee_Id), 'w') as file:
+        json.dump(dict, file)
