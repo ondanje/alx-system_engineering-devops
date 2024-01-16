8662617557b3020adba29af86f114a8f67b2ad7c
ondanje <cyrilondanje@gmail.com>
2024-01-16 19:58:54 +0300

    no. of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,21 +8,12 @@ import requests
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    headers = {'User-Agent': 'CustomUserAgent/1.0'}
+    headers = {'User-Agent': 'User Agent 1'}
 
-    try:
-        response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-
-        elif response.status_code == 404:
-            return 0
-
-        else:
-            print(f"Error: {response.status_code}")
-            return 0
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
